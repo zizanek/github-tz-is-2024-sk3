e5701eaa880d8f095f57ee6e865c8a6f09442443
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-03 17:48:58 +0200

    #1 prvni program - uzivatelsky vstup

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program {
+
+    static void Main() {
+        Console.Clear();
+        Console.WriteLine("****************************");
+        Console.WriteLine("**** Výpis číselné řady ****");
+        Console.WriteLine("******* Tomáš Žižka ********");
+        Console.WriteLine("****************************");
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // Vstup od uživatele
+        /* Console.Write("Zadejte první číslo (celé číslo): ");
+        int first = int.Parse(Console.ReadLine()); */
+        
+        // Vstup od uživatele  - lépe řešený
+        Console.Write("Zadejte první číslo (celé číslo): ");
+        int first;
+        while(!int.TryParse(Console.ReadLine(), out first)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+        }
+        
+        Console.ReadKey();
+    }
+} 
+
+
+
