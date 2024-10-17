cfb90165511f5cd1e0aac59d023e186a0ba3fe59
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-17 16:40:22 +0200

    #4 soucet cifer - zakladni kod

---
+++ IS-Projekty/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,31 @@
+﻿string again = "a";
+        while(again == "a") {
+
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("**** Výpis číselné řady ****");
+            Console.WriteLine("******* Tomáš Žižka ********");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Zadejte číslo (celé číslo): ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo (celé číslo): ");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("Číslo: {0}", number);
+            Console.WriteLine("========================================================");
+            
+            
+           
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine();    
+
+        }
