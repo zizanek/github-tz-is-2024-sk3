b8d3d545cdae581116047120255649b4611b05ea
Tomáš Žižka <tomas.zizka@tul.cz>
2024-10-24 16:34:04 +0200

    #6 Generátor - vstup

--- IS-Projekty/program004-generator/Program.cs
+++ IS-Projekty/program004-generator/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+        while(again == "a") {
+
+            Console.Clear();
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("**** Generátor pseudonáhodných čísel ****");
+            Console.WriteLine("*************** Tomáš Žižka *************");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu (celé číslo): ");
+            }
+
+            Console.Write("Zadejte dolní mez (celé číslo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu (celé číslo): ");
+            }
+
+            Console.Write("Zadejte horní mez (celé číslo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu (celé číslo): ");
+            }
+
+
+
+            
+            Console.WriteLine();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
+            Console.WriteLine("========================================================\n\n");
+            
+            
+            
+
+
+           
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine();    
+
+        }
